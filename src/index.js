fcc9643000bad3479077c4ac7f916937781857ce
vikysan77 <vikysan@outlook.com>
2021-08-17 20:04:16 +0300

    feat: reverse-int task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse (x) {
+    var str = x.toString(); 
+    var strArray = str.split(""); 
+    var revArray = strArray.reverse(); 
+    var rev = revArray.join(""); 
+    return parseInt(rev);
 }
